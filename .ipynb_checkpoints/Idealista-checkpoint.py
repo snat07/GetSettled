31249a943428742c6e23b3c4516c82393dd03d3d
Sebastian Natalevich <snat07@gmail.com>
2019-04-30 11:00:27 +0200

    Idealista script it's working

--- .ipynb_checkpoints/Idealista-checkpoint.py
+++ .ipynb_checkpoints/Idealista-checkpoint.py
@@ -16,6 +16,7 @@ class Idealista:
     longitude = 'longitude'
     distance = 'distance'
     priceByArea = 'priceByArea'
+    neighborhoods = {}
 
    
 
@@ -35,10 +36,11 @@ class Idealista:
         r = requests.post('http://api.idealista.com/oauth/token',
                       headers={'Authorization': 'Basic ' + encoded.decode()},
                       params={'grant_type':'client_credentials'})
-
+        print(r)
         access_token = r.json()['access_token']
         token_type = r.json()['token_type']
         self.search_authorization =  token_type + ' ' + access_token
+        
 
 
 #    country = 'es'
@@ -53,6 +55,7 @@ class Idealista:
 
 
     def request_idealista(self,locale, maxItems, center, distance, propertyType):
+
         params = {
             'locale':locale,
             'maxItems':maxItems,
@@ -62,35 +65,56 @@ class Idealista:
             'center' :center,
             'distance' :distance
         }
+        
         search = requests.post('https://api.idealista.com/3.5/es/search',
-                          headers={'authorization_header': self.search_authorization},
+                          headers={'Authorization': self.search_authorization},
                           params=params)
         return search.json().get('elementList')
 
+    
+    def add_item_to_dict(self,home):
+        item = { self.price:home[self.price],
+                self.propertyType:home[self.propertyType],
+                self.operation:home[self.operation],
+                self.rooms:home[self.rooms],
+                self.latitude:home[self.latitude],
+                self.longitude:home[self.longitude],
+                self.distance:home[self.distance],
+                self.priceByArea:home[self.priceByArea]
+            }
+
+        if home[self.neighborhood] not in self.neighborhoods:
+            self.neighborhoods[home[self.neighborhood]] = []
+
+        self.neighborhoods[home[self.neighborhood]].append(item)
 
 
-    def get_neighborhoods(self, center, distance, locale='en', maxItems=50, propertyType='homes'):
-        print(center)
-        homes = self.request_idealista(locale, maxItems, center, distance, propertyType)
-        neighborhoods = {}
+    
+    def load_homes(self, center, distance, locale='en', maxItems=50):
+        homes = self.request_idealista(locale, maxItems, center, distance, 'homes')
+        bedrooms = self.request_idealista(locale, maxItems, center, distance, 'bedrooms')
         for home in homes:
-            item = { price:home[price],
-                    propertyType:home[propertyType],
-                    price:home[price],
-                    operation:home[operation],
-                    rooms:home[rooms],
-                    latitude:home[latitude],
-                    longitude:home[longitude],
-                    distance:home[distance],
-                    priceByArea:home[priceByArea]
-                }
+            self.add_item_to_dict(home)
+        for bedroom in bedrooms:
+            self.add_item_to_dict(bedroom)
 
-        if home[neighborhood] not in neighborhoods:
-            neighborhoods[home[neighborhood]] = []
             
-            neighborhoods[home[neighborhood]].append(item)
-        return neighborhoods
+    def price_of_neighborhoods(self):
+        neighs_price = {}
+        for key in self.neighborhoods.keys():
+            neighs_price[key] = self.price_per_neighborhood(key)
+        return neighs_price
+    
+    def price_per_neighborhood(self,key):
+        price_per_neighborhood = 0
+        total_number_of_rooms = 0
+        for appartment in self.neighborhoods[key]:
+            if appartment[self.propertyType] == 'room':
+                price_per_neighborhood += appartment[self.price]
+                total_number_of_rooms += 1
+            else:
+                number_of_rooms = appartment[self.rooms] if appartment[self.rooms] > 0 else 1
+                price_per_neighborhood += appartment[self.price] / number_of_rooms
+                total_number_of_rooms += appartment[self.rooms]
+        return (price_per_neighborhood / len(self.neighborhoods[key]), total_number_of_rooms)
 
-#idealista = Idealista()
-#idealista.create()
-#print(idealista.get_neighborhoods(center='41.38621,2.15487',distance=1500))
